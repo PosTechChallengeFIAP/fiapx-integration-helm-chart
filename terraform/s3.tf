ea6b48f154ffd7a2448f66095780cf455a4778e0
Guilherme Teixeira <guisebastian15@gmail.com>
2025-06-26 01:05:47 -0300

    Add terraform

---
+++ terraform/s3.tf
@@ -0,0 +1,53 @@
+
+
+resource "aws_s3_bucket" "output_bucket" {
+  bucket = "fiapx-resulted-zipfiles"
+
+  tags = {
+    Name        = "Output"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "output_bucket" {
+  bucket = aws_s3_bucket.output_bucket.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "output_bucket" {
+  bucket = aws_s3_bucket.output_bucket.id
+  acl    = "private"
+
+  depends_on = [
+    aws_s3_bucket_ownership_controls.output_bucket
+  ]
+}
+
+resource "aws_s3_bucket" "uploads_bucket" {
+  bucket = "fiapx-uploaded-videos"
+
+  tags = {
+    Name        = "Uploads"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "uploads_bucket" {
+  bucket = aws_s3_bucket.uploads_bucket.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "uploads_bucket" {
+  bucket = aws_s3_bucket.uploads_bucket.id
+  acl    = "private"
+
+  depends_on = [
+    aws_s3_bucket_ownership_controls.uploads_bucket
+  ]
+}
