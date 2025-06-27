a33bbbdb65eebc747f9ce73ac10d345d704e4c14
Guilherme Teixeira <guisebastian15@gmail.com>
2025-06-27 15:16:31 -0300

    fix stage name

--- terraform/integration/gateway.tf
+++ terraform/integration/gateway.tf
@@ -34,6 +34,12 @@ resource "aws_api_gateway_integration" "proxy_integration" {
 
 resource "aws_api_gateway_deployment" "eks_deployment" {
   rest_api_id = aws_api_gateway_rest_api.eks_rest_api.id
-  stage_name  = "prod"
+
   depends_on  = [aws_api_gateway_integration.proxy_integration]
+}
+
+resource "aws_api_gateway_stage" "eks_stage" {
+  stage_name    = "prod"
+  rest_api_id   = aws_api_gateway_rest_api.eks_rest_api.id
+  deployment_id = aws_api_gateway_deployment.eks_deployment.id
 }
