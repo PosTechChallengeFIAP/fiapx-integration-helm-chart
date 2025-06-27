246c42f20aa044291eb56a6a1db4ed24aaa37ac3
Guilherme Teixeira <guisebastian15@gmail.com>
2025-06-27 16:17:22 -0300

    fix stage name

--- terraform/integration/gateway.tf
+++ terraform/integration/gateway.tf
@@ -25,7 +25,25 @@ resource "aws_api_gateway_integration" "proxy_integration" {
   rest_api_id             = aws_api_gateway_rest_api.eks_rest_api.id
   resource_id             = aws_api_gateway_resource.proxy.id
   http_method             = aws_api_gateway_method.proxy_method.http_method
-  integration_http_method = "GET"
+  integration_http_method = "ANY"
+  type                    = "HTTP_PROXY"
+  uri                     = "http://${var.nlb_dns}:80"
+  connection_type         = "VPC_LINK"
+  connection_id           = aws_api_gateway_vpc_link.eks_vpc_link.id
+}
+
+resource "aws_api_gateway_method" "root_method" {
+  rest_api_id   = aws_api_gateway_rest_api.eks_rest_api.id
+  resource_id   = aws_api_gateway_rest_api.eks_rest_api.root_resource_id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "root_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.eks_rest_api.id
+  resource_id             = aws_api_gateway_rest_api.eks_rest_api.root_resource_id
+  http_method             = aws_api_gateway_method.root_method.http_method
+  integration_http_method = "ANY"
   type                    = "HTTP_PROXY"
   uri                     = "http://${var.nlb_dns}:80"
   connection_type         = "VPC_LINK"
