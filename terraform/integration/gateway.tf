b2a45375677330f0e8d3e12c42178898a4875b5e
Guilherme Teixeira <guisebastian15@gmail.com>
2025-06-27 23:23:27 -0300

    add {proxy} to uri

--- terraform/integration/gateway.tf
+++ terraform/integration/gateway.tf
@@ -43,7 +43,7 @@ resource "aws_api_gateway_integration" "proxy_get_integration" {
   http_method             = "GET"
   integration_http_method = "GET"
   type                    = "HTTP_PROXY"
-  uri                     = "http://${var.nlb_dns}:80"
+  uri                     = "http://${var.nlb_dns}:80/{proxy}"
   connection_type         = "VPC_LINK"
   connection_id           = aws_api_gateway_vpc_link.eks_vpc_link.id
 
@@ -56,7 +56,7 @@ resource "aws_api_gateway_integration" "proxy_post_integration" {
   http_method             = "POST"
   integration_http_method = "POST"
   type                    = "HTTP_PROXY"
-  uri                     = "http://${var.nlb_dns}:80"
+  uri                     = "http://${var.nlb_dns}:80/{proxy}"
   connection_type         = "VPC_LINK"
   connection_id           = aws_api_gateway_vpc_link.eks_vpc_link.id
 
@@ -69,73 +69,11 @@ resource "aws_api_gateway_integration" "proxy_put_integration" {
   http_method             = "PUT"
   integration_http_method = "PUT"
   type                    = "HTTP_PROXY"
-  uri                     = "http://${var.nlb_dns}:80"
+  uri                     = "http://${var.nlb_dns}:80/{proxy}"
   connection_type         = "VPC_LINK"
   connection_id           = aws_api_gateway_vpc_link.eks_vpc_link.id
 
   depends_on = [aws_api_gateway_method.proxy_put]
-}
-
-# Methods
-resource "aws_api_gateway_method" "root_get" {
-  rest_api_id   = aws_api_gateway_rest_api.eks_rest_api.id
-  resource_id   = aws_api_gateway_rest_api.eks_rest_api.root_resource_id
-  http_method   = "GET"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_method" "root_post" {
-  rest_api_id   = aws_api_gateway_rest_api.eks_rest_api.id
-  resource_id   = aws_api_gateway_rest_api.eks_rest_api.root_resource_id
-  http_method   = "POST"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_method" "root_put" {
-  rest_api_id   = aws_api_gateway_rest_api.eks_rest_api.id
-  resource_id   = aws_api_gateway_rest_api.eks_rest_api.root_resource_id
-  http_method   = "PUT"
-  authorization = "NONE"
-}
-
-# Integrations
-resource "aws_api_gateway_integration" "root_get_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.eks_rest_api.id
-  resource_id             = aws_api_gateway_rest_api.eks_rest_api.root_resource_id
-  http_method             = "GET"
-  integration_http_method = "GET"
-  type                    = "HTTP_PROXY"
-  uri                     = "http://${var.nlb_dns}:80"
-  connection_type         = "VPC_LINK"
-  connection_id           = aws_api_gateway_vpc_link.eks_vpc_link.id
-
-  depends_on = [aws_api_gateway_method.root_get]
-}
-
-resource "aws_api_gateway_integration" "root_post_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.eks_rest_api.id
-  resource_id             = aws_api_gateway_rest_api.eks_rest_api.root_resource_id
-  http_method             = "POST"
-  integration_http_method = "POST"
-  type                    = "HTTP_PROXY"
-  uri                     = "http://${var.nlb_dns}:80"
-  connection_type         = "VPC_LINK"
-  connection_id           = aws_api_gateway_vpc_link.eks_vpc_link.id
-
-  depends_on = [aws_api_gateway_method.root_post]
-}
-
-resource "aws_api_gateway_integration" "root_put_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.eks_rest_api.id
-  resource_id             = aws_api_gateway_rest_api.eks_rest_api.root_resource_id
-  http_method             = "PUT"
-  integration_http_method = "PUT"
-  type                    = "HTTP_PROXY"
-  uri                     = "http://${var.nlb_dns}:80"
-  connection_type         = "VPC_LINK"
-  connection_id           = aws_api_gateway_vpc_link.eks_vpc_link.id
-
-  depends_on = [aws_api_gateway_method.root_put]
 }
 
 resource "aws_api_gateway_deployment" "eks_deployment" {
@@ -144,10 +82,7 @@ resource "aws_api_gateway_deployment" "eks_deployment" {
   depends_on = [
     aws_api_gateway_integration.proxy_get_integration,
     aws_api_gateway_integration.proxy_post_integration,
-    aws_api_gateway_integration.proxy_put_integration,
-    aws_api_gateway_integration.root_get_integration,
-    aws_api_gateway_integration.root_post_integration,
-    aws_api_gateway_integration.root_put_integration
+    aws_api_gateway_integration.proxy_put_integration
   ]
 }
 
