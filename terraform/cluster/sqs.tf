937dc77c8b7a605b4469f02c04e8cd1b299e50cf
Guilherme Teixeira <guisebastian15@gmail.com>
2025-07-03 01:03:31 -0300

    Create SQS

---
+++ terraform/cluster/sqs.tf
@@ -0,0 +1,16 @@
+resource "aws_sqs_queue" "processing-request-queue" {
+  name                      = "processing-request-queue"
+  delay_seconds             = 0
+  max_message_size          = 262144
+  message_retention_seconds = 86400
+  receive_wait_time_seconds = 10
+
+  tags = {
+    Environment = "dev"
+    Name        = "processing-request-queue"
+  }
+}
+
+output "sqs_queue_url" {
+  value = aws_sqs_queue.processing-request-queue.url
+}
