609e828fea47c67630d7598bb3f9aa4fdcdde079
Guilherme Teixeira <guisebastian15@gmail.com>
2025-07-05 01:09:14 -0300

    s3 names

--- terraform/cluster/s3.tf
+++ terraform/cluster/s3.tf
@@ -1,7 +1,7 @@
 
 
 resource "aws_s3_bucket" "output_bucket" {
-  bucket = "fiapx-resulted-zipfiles-3"
+  bucket = "fiapx-resulted-zipfiles-d27d8e32-c0bd-4f5c-8b97-dd1310b3a289"
 
   tags = {
     Name        = "Output"
@@ -18,7 +18,7 @@ resource "aws_s3_bucket_ownership_controls" "output_bucket" {
 }
 
 resource "aws_s3_bucket" "uploads_bucket" {
-  bucket = "fiapx-uploaded-videos-3"
+  bucket = "fiapx-uploaded-videos-d27d8e32-c0bd-4f5c-8b97-dd1310b3a289"
 
   tags = {
     Name        = "Uploads"
